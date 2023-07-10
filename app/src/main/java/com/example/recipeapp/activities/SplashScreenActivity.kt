47339e807f8175513df09764b04e7f2550f9475d
Vishal Chand <sam.chand96@gmail.com>
2023-07-10 17:48:01 +0530

    Finished with add the fragments and adding the completed the navigation drawer functionality.

--- app/src/main/java/com/example/recipeapp/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/example/recipeapp/activities/SplashScreenActivity.kt
@@ -1,15 +1,43 @@
 package com.example.recipeapp.activities
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.ui.AppBarConfiguration
+import com.example.recipeapp.R
 import com.example.recipeapp.databinding.ActivitySplashScreenBinding
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
 
-private lateinit var binding: ActivitySplashScreenBinding
+    private lateinit var binding: ActivitySplashScreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
     }
+
+    private fun init() {
+        startAnimation()
+        Handler(mainLooper).postDelayed({
+            val homeIntent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(homeIntent)
+        }, 4000)
+    }
+
+    private fun startAnimation() {
+        binding.appNameTv.startAnimation(
+            AnimationUtils.loadAnimation(
+                applicationContext,
+                R.anim.custom_animation
+            )
+        )
+        binding.appNameTv.animate().start()
+    }
+
+
 }
