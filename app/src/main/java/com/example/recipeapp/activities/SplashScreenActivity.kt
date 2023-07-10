1e7ca3b179d8a0bfe1033fac5cc73f0acc11a600
Vishal Chand <sam.chand96@gmail.com>
2023-07-10 14:13:38 +0530

    initial commit.

---
+++ app/src/main/java/com/example/recipeapp/activities/SplashScreenActivity.kt
@@ -0,0 +1,15 @@
+package com.example.recipeapp.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import com.example.recipeapp.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+
+private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/recipeapp/utils/VolleyRequests.kt
@@ -0,0 +1,4 @@
+package com.example.recipeapp.utils
+
+class VolleyRequests {
+}
