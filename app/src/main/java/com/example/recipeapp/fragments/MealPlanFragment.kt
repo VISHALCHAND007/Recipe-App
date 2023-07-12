43b1f70c0489d27f9bcc07f76fa0095ec9e406d7
Vishal Chand <sam.chand96@gmail.com>
2023-07-12 20:28:57 +0530

    Added the title to the weekly meal plan.

--- app/src/main/java/com/example/recipeapp/fragments/MealPlanFragment.kt
+++ app/src/main/java/com/example/recipeapp/fragments/MealPlanFragment.kt
@@ -1,5 +1,6 @@
 package com.example.recipeapp.fragments
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,17 +9,21 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.recipeapp.adapter.ShoppingListAdapter
 import com.example.recipeapp.adapter.WeeklyListAdapter
 import com.example.recipeapp.databinding.FragmentMealPlanBinding
 import com.example.recipeapp.room.entity.WeeklyPlanEntity
 import com.example.recipeapp.viewModel.WeeklyMealViewModel
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
 
 class MealPlanFragment : Fragment() {
     private lateinit var binding: FragmentMealPlanBinding
     private lateinit var mContext: Context
     private lateinit var viewModel: WeeklyMealViewModel
     private lateinit var adapter: WeeklyListAdapter
+    private lateinit var title: String
+    private lateinit var mCalendar: Calendar
 
 
     override fun onCreateView(
@@ -33,12 +38,27 @@ class MealPlanFragment : Fragment() {
 
     private fun init() {
         initTasks()
+        setTitle()
         getData()
+    }
+
+    private fun setTitle() {
+        val startDate = mCalendar.time
+        mCalendar.add(Calendar.DATE, 7)
+        val endDate = mCalendar.time
+
+        val startDateStr = dateFormatter(startDate)
+        val endDateStr = dateFormatter(endDate)
+
+        title = "Weekly Plan (${startDateStr}-${endDateStr})"
+
+        binding.titleTv.text = title
     }
 
     private fun initTasks() {
         mContext = requireContext()
         viewModel = ViewModelProvider(requireActivity())[WeeklyMealViewModel::class.java]
+        mCalendar = Calendar.getInstance()
     }
 
     private fun getData() {
@@ -63,5 +83,10 @@ class MealPlanFragment : Fragment() {
             binding.noResultFoundTv.visibility = View.VISIBLE
             binding.weeklyShoppingListRv.visibility = View.GONE
         }
+    }
+    @SuppressLint("SimpleDateFormat")
+    private fun dateFormatter(date: Date): String {
+        val formatter = SimpleDateFormat("dd/MM/yyyy")
+        return formatter.format(date)
     }
 }
